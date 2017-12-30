cfec4eafef2e4b6b95cf5c3f7f5557aba38dc72e
rtarvids <33689808+rtarvids@users.noreply.github.com>
2017-12-30 14:06:31 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='3Dfunctiongrapher',
+    version='0.1dev',
+    url='https://github.com/rtarvids/3D-Function-Grapher',
+    install_requires=['Matplotlib>=2.0.0', 'Numpy>=1.11.3'],
+    packages=['3dfunctiongrapher'],
+    platforms='any',
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
