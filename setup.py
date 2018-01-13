7b428e0705f7b27de8945d93560d13f3b3581acb
rtarvids <33689808+rtarvids@users.noreply.github.com>
2018-01-13 13:39:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='3Dfunctiongrapher',
     version='0.1dev',
-    author='rtarvids'
+    author='rtarvids',
     url='https://github.com/rtarvids/3D-Function-Grapher',
     install_requires=['Matplotlib>=2.0.0', 'Numpy>=1.11.3'],
     packages=['3dfunctiongrapher'],
